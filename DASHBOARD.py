f4fa76db31074adfe2c73bbc7515aec7ecb0fa4f
Aadhya Subhash <117385752+aadhyea@users.noreply.github.com>
2024-07-16 13:46:38 +0530

    Update DASHBOARD.py

--- DASHBOARD.py
+++ DASHBOARD.py
@@ -11,18 +11,66 @@ def encode_image(image_file):
     return 'data:image/png;base64,{}'.format(encoded.decode())
 
 app.layout = html.Div([
-    html.H1('Dashboard of Plots'),
+    html.H1('Dashboard of Plots', style={'color': 'Brown', 'text-align':'center', 'font-size':'100px', 'text-decoration':'underline'}),
     html.Div([
-        html.H2('Box Plot of BMI by Diabetes_012'),
-        html.Img(src=encode_image('Images\\BoPlot-BMI vs Target.png'))
+        html.H2('Box Plot of BMI by Diabetes_012', style={'text-align':'center'}),
+        html.Img(src=encode_image('Images\\BoPlot-BMI vs Target.png'), style = {
+            'padding': '20px',
+            'margin': '10px',
+            'border': '1px solid #ccc',
+            'background-color': 'grey',
+            'margin-left':'140px'
+        })
     ]),
     html.Div([
-        html.H2('Bar Plot of BMI by Diabetes_012'),
-        html.Img(src=encode_image('Images\BarPlot-BMI vs Target.png'))
+        html.H2('Bar Plot of BMI by Diabetes_012', style={'text-align':'center'}),
+        html.Img(src=encode_image('Images\BarPlot-BMI vs Target.png'), style = {
+            'padding': '20px',
+            'margin': '10px',
+            'border': '1px solid #ccc',
+            'background-color': 'grey',
+            'margin-left':'150px'
+        })
     ]),
     html.Div([
-        html.H2('Stacked Bar plot of individuals with Diabetes, if they have BP or Cholestrol'),
-        html.Img(src=encode_image('Images\\StackedBar.png'))
+        html.H2('Stacked Bar plot of individuals with Diabetes, if they have BP or Cholestrol', style={'text-align':'center'}),
+        html.Img(src=encode_image('Images\\StackedBar.png'), style = {
+            'padding': '20px',
+            'margin': '10px',
+            'border': '1px solid #ccc',
+            'background-color': 'grey',
+            'margin-left':'220px'
+        })
+    ]),
+    html.Div([
+        html.H2('Histogram distribution of age', style={'text-align':'center'}),
+        html.Img(src=encode_image('Images\\Hist-Age.png'), style = {
+            'padding': '20px',
+            'margin': '10px',
+            'border': '1px solid #ccc',
+            'background-color': 'grey',
+            'margin-left':'390px'
+        })
+    ]),
+    html.Div([
+        html.H2('Histogram of BMI(Body mass index)', style={'text-align':'center'}),
+        html.Img(src=encode_image('Images\\Hist-BMI.png'), style = {
+            'padding': '20px',
+            'margin': '10px',
+            'border': '1px solid #ccc',
+            'background-color': 'grey',
+            'margin-left':'390px',
+        })
+    ]),
+    html.Div([
+        html.H2('Distribution of Physical-Health of Diabetic Individuals', style={'text-align':'center'}),
+        html.Img(src=encode_image('Images\\Physical-Health.png'), style = {
+            'padding': '20px',
+            'margin': '10px',
+            'border': '1px solid #ccc',
+            'background-color': 'grey',
+            'margin-left':'390px'
+        })
     ])
 ])
 
